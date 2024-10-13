822fbd96afe5f300bb8ddfc5578c94af2e1bd4c3
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-10-13 06:30:02 -0400

    Update 2011. Final Value of Variable After Performing Operations.java

--- 2011. Final Value of Variable After Performing Operations.java
+++ 2011. Final Value of Variable After Performing Operations.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+        for (String op : operations) {
+            switch (op.charAt(1)) {
+                case '+': X++; break;
+                case '-': X--; break;
+            }
+        }
+        return X;
+    }
+}
